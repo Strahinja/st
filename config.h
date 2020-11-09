f3d9ee152d1a926ec821f7b6d761a06b556f8dcf
Страхиња Радић <contact@strahinja.org>
2020-11-09 20:02:58 +0100

    Changed font to Terminus + Noto Color Emoji

--- config.h
+++ config.h
@@ -5,9 +5,11 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "PxPlus_IBM_VGA8 Nerd Font:size=12:style=Book:antialias=false:autohint=true";
+static char *font = "xos4 Terminus:size=10:style=Regular:antialias=false:autohint=true";
+//static char *font = "PxPlus_IBM_VGA8 Nerd Font:size=12:style=Book:antialias=false:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
+        "Noto Color Emoji:style=Regular:size=10"
 /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
 /*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
 };
