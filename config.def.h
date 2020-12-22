7e23d6575bf311f34de1a350c811c08af641eea4
Страхиња Радић <contact@strahinja.org>
2020-12-22 21:49:21 +0100

    Reverted and removed scrollback* patches (replaced by tmux)
    Signed-off-by: Страхиња Радић <contact@strahinja.org>

--- config.def.h
+++ config.def.h
@@ -193,8 +193,6 @@ static uint forcemousemod = ShiftMask;
  */
 static MouseShortcut mshortcuts[] = {
 	/* mask                 button   function        argument       release */
-	{ XK_ANY_MOD,           Button4, kscrollup,      {.i = 1},      0, /* !alt */ -1 },
-	{ XK_ANY_MOD,           Button5, kscrolldown,    {.i = 1},      0, /* !alt */ -1 },
 	{ XK_ANY_MOD,           Button2, clippaste,       {.i = 0},      1 },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
@@ -225,8 +223,6 @@ static Shortcut shortcuts[] = {
 	{ XK_ANY_MOD,		XK_Menu,	externalpipe,	{.v = openurlcmd } },
 	{ ShiftMask,            XK_Insert,      clippaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
